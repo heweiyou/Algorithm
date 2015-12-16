bbbcc2f9335f02b0e373aef8d567416d6e13eb9e
heweiyou <heweiyou@aliyun.com>
2015-12-17 03:50:13 +0800

    add print for MatrixChainMultiplication

--- DynamicProgramming/MatrixChainMultiplication.c
+++ DynamicProgramming/MatrixChainMultiplication.c
@@ -3,11 +3,49 @@
 #define MAX 105
 #define INF 0x7fffffff
 
-int MatrixChainMultiplication(int *num,int n)
+void printParentheses(int path[][MAX],int l,int r)
 {
-	int dp[MAX][MAX];
-	int i,j,k,d;
+	int k;
+
+	if(l == r)
+	{
+		printf("A%d",l);
+		return ;
+	}else if(r - l == 1)
+	{
+		printf("A%dA%d",l,r);
+		return ;
+	}else if(l > r)
+	{
+		return ;
+	}
 	
+	k = path[l][r];
+	if(l < k)
+	{
+		printf("(");
+		printParentheses(path,l,k);
+		printf(")");
+	}else if(l == k)
+	{
+		printParentheses(path,l,k);
+	}
+
+	if(k+1 < r)
+	{
+		printf("(");
+		printParentheses(path,k+1,r);
+		printf(")");
+	}else if(k+1 == r)
+	{
+		printParentheses(path,k+1,r);
+	}
+}
+
+void MatrixChainMultiplication(int *num,int n,int dp[][MAX],int path[][MAX])
+{
+	int i,j,k,d;
+		
 	for(i = 1 ; i <= n; i++)
 	{
 		dp[i][i] = 0;
@@ -24,12 +62,11 @@ int MatrixChainMultiplication(int *num,int n)
 				if(dp[i][j] > dp[i][k-1]+dp[k][j] + num[i]*num[k]*num[j+1])
 				{
 					dp[i][j] = dp[i][k-1]+dp[k][j] + num[i]*num[k]*num[j+1];
+					path[i][j] = k-1;
 				}
 			}
 		}
 	}
-
-	return dp[1][n];
 
 }
 
@@ -38,6 +75,8 @@ int main()
 	int num[MAX];
 	int n;
 	int i;
+	int dp[MAX][MAX];
+	int path[MAX][MAX];
 
 	while(scanf("%d",&n) != EOF && n)
 	{
@@ -45,8 +84,10 @@ int main()
 		{
 			scanf("%d",&num[i]);
 		}
-
-		printf("The num of multiplication is %d\n",MatrixChainMultiplication(num,n));
+		MatrixChainMultiplication(num,n,dp,path);
+		printf("The num of multiplication is %d\n",dp[1][n]);
+		printParentheses(path,1,n);
+		printf("\n");
 	}
 	return 0;
 }
